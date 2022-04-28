f800afce67edd24cd517f2ba2c596445f0a4cfd1
Gaurav Narnaware <gauravnarnaware3112003@gmail.com>
2022-04-28 16:52:30 +0530

    Edit auth.jaava

--- src/UserAuthentication/auth.java
+++ src/UserAuthentication/auth.java
@@ -3,7 +3,9 @@ package UserAuthentication;
 import UserAuthentication.RegisterLogin.Register.MainData.MainData;
 import UserAuthentication.RegisterLogin.Register.Register;
 import UserAuthentication.RegisterLogin.*;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class auth extends Register {
@@ -14,6 +16,39 @@ public class auth extends Register {
   Login L1 = new Login();
   Register[] u1 = new Register[100];
   MainData Secure = new MainData();
+
+  // for Registration
+  public void RegisterNow() {
+    try {
+
+      File Obj = new File("allUsers.txt");
+      if (Obj.createNewFile()) {
+        System.out.println("File " + Obj.getName() + " is created successfully.");
+      } else {
+        System.out.println("File is already exist in the directory.");
+      }
+
+      System.out.println("********** Register Now **********");
+      System.out.println();
+        u1[user] = new auth();
+        u1[user].getData();
+
+        FileWriter Writer = new FileWriter(Obj);
+        for (int i = 0; i < user; i++) {
+          Writer.write(user + " " + u1[user].Name + "\n");
+          Writer.write(user + " " + u1[user].uName + "\n");
+          Writer.write(user + " " + u1[user].eMail + "\n");
+          Writer.close();
+          System.out.println("Successfully written.");
+        }
+        user = user + 1;
+        
+
+    } catch (IOException e) {
+      System.out.println("Internal Server Error");
+      e.printStackTrace();
+    }
+  }
 
   // for User Login
   public void userLogin() {
@@ -30,17 +65,6 @@ public class auth extends Register {
       System.out.println("Login Successfull");
     } else {
       System.out.println("Invalid Credential");
-    }
-  }
-
-  // for Registration
-  public void RegisterNow() {
-    System.out.println("********** Register Now **********");
-    System.out.println();
-    for (int i = 0; i < 1; i++) {
-      u1[user] = new auth();
-      u1[user].getData();
-      user = user + 1;
     }
   }
 
