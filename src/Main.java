0de08e6692b68c6552fcbc2f9ff8ae5126edf91a
Gaurav Narnaware <gauravnarnaware3112003@gmail.com>
2022-05-07 23:31:44 +0530

    next1

--- src/Main.java
+++ src/Main.java
@@ -6,34 +6,7 @@ public class Main {
   public static void main(String[] args) {
     MainAcce a1 = new MainAcce();
     a1.start();
-    // Scanner sc = new Scanner(System.in);
-    // int choice;
-    // auth A1 = new auth();
-    // do {
-    //   System.out.println("********** USER AUTHENTICATION SYSTEM **********");
-    //   System.out.println("1. Register \n2. Login \n3. Display User \n4. Exit");
-    //   System.out.print("Enter the choice : ");
-    //   choice = sc.nextInt();
-    //   System.out.println();
-    //   sc.nextLine();
-    //   System.out.println();
-    //   switch (choice) {
-    //     case 1:
-    //       A1.RegisterNow();
-    //       break;
-    //     case 2:
-    //       A1.userLogin();
-    //       break;
-    //     case 3:
-    //       A1.adminLogin();
-    //       break;
-    //     case 4:
-    //       System.out.println();
-    //       break;
-    //     default:
-    //       System.out.println("Inavlid Selection");
-    //   }
-    // } while (choice != 4);
+  
 
   }
 }
